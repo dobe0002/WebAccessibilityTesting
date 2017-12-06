65bb825356b21a306f0686e2da2938e8f0443b18
Kim Doberstein (UMN) <oit-dobe0002@x-160-94-33-19.oit.umn.edu>
2017-12-06 14:35:36 -0600

    First commit

---
+++ assets/javascripts/app.js
@@ -0,0 +1,58 @@
+/************** Document load ************/
+
+$(".toggle-warnings-btn").click(function(){$(".alert-warning").toggle("slow")});
+
+
+/*************** MODAL *******************/
+var modalInitObject;
+$('.modal').on('show.bs.modal',function(e){
+	//modalInitObject=$(e.relatedTarget);
+	modalInitObject=e.relatedTarget;
+});
+$('.modal').on('shown.bs.modal',function(e){
+	$('.modal-header .close').focus();
+})
+$('.modal').on('hidden.bs.modal', function (e) {
+	if(modalInitObject!==''){
+		$(modalInitObject).focus();
+		modalInitObject='';
+	}
+})
+
+
+function openModal(options){
+  var settings = {
+        'body':'' 
+        ,'title':'' 
+        ,'footer':'' 
+        ,'size':'medium' 
+        ,'setForShow':function(){} 
+
+        /* Not normally passed - set here for easy access or if multiple modals are needed */
+        ,'modalBodyObj':$('.modal-body')
+        ,'modalTitleObj':$('.modal-title')
+        ,'modalFooterObj':$('.modal-footer')
+        ,'modalObj':$('.modal')
+    };
+
+    for (var key in options) {if(options[key]!=undefined)settings[key]=options[key]} 
+  
+    /* resize modal if needed */
+    var modalDoc=settings.modalObj.find('.modal-dialog');
+    $(modalDoc).removeClass('modal-lg').removeClass('modal-sm');
+    if(settings.size=='small'){$(modalDoc).addClass('modal-sm');}
+    if(settings.size=='large'){$(modalDoc).addClass('modal-lg');}
+
+    /* set content */
+    $(settings.modalTitleObj).html(settings.title);
+   	$(settings.modalBodyObj).html(settings.body);
+   	$(settings.modalFooterObj).html(settings.footer);
+
+   	/* hide/show title and footer as needed */
+   	if(settings.title=='')$(settings.modalTitleObj).hide();
+   	else $(settings.modalTitleObj).show();
+   	if(settings.footer=='')$(settings.modalFooterObj).hide();
+   	else $(settings.modalFooterObj).show();
+    
+     settings.modalObj.modal('show');
+}
